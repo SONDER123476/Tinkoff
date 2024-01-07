a6bcc68728c3060288f44bf9d008eeaf31173630
SONDER123476 <102980464+SONDER123476@users.noreply.github.com>
2024-01-08 00:05:22 +0400

    Исправленное

--- main.js
+++
@@ -1,9 +0,0 @@
-
-/*const sectionlist = document.querySelector('')*/
-const list = document.querySelector('.ics-white-panel');
-
-list.addEventListener('click', (evt)=>{
-    evt.preventDefault();
-    const contetnt = evt.target.parentNode.nextElementSibling.lastElementChild;
-    contetnt.classList.toggle('visible');
-});
